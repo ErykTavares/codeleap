f55776fbba6eeaea32a8c5a15a96d9736a2f3676
ErykTavares <erictavares35@gmail.com>
2022-04-08 11:47:41 -0300

    useEffect waring fix

--- src/components/Modal/ModalEdit/index.jsx
+++ src/components/Modal/ModalEdit/index.jsx
@@ -36,7 +36,7 @@ const ModalEdit = ({
   React.useEffect(() => {
     titleinput.setValue(postedit.title);
     content.setValue(postedit.content);
-  },[]);
+  });
 
   async function handleSubmit(event) {
     event.preventDefault();
